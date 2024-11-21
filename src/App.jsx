28c0feca9652b1449149129f3af3a5c754cfe767
Vanshaj Rao <vanshaj962@gmail>
2024-11-21 11:09:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Manager from './components/manager'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+        <Navbar/>
+        <Manager/>
+        <Footer/>
+     
+    </>
+  )
+}
+
+export default App
